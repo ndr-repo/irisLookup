4c726495c508acc5019dce2bfc29ea04ca0ad841
weekndr_sec <172009322+ndr-repo@users.noreply.github.com>
2026-02-20 20:08:56 -0500

    Refactor irisLookup.py for improved logging and structure

--- irisLookup.py
+++ irisLookup.py
@@ -1,99 +1,69 @@
 import requests
+import random
 import sys
-print(' ')
-print('discoveryOps: Iris Username Lookup')
-print(' ')
-print('Created by Gabriel H. @weekndr_sec')
+#
+# CLI Wizard - replace var with comment:
+# targetUser = input("Enter the target username: ")
+#
+targetUser = str(sys.argv[1]).strip()
+user_agents = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+               'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+               'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+               'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
+               'Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15']
+for i in user_agents:
+    headers = {'User-Agent': random.choice(user_agents)}
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+#
+# If you want to inspect with a proxy tool (ie: mitmproxy):
+# proxies = {"https": "http://localhost:8080"}
+#
+print("  ")
+print(str("irisLookup - Username Search Tool for OSINT"))
+print(" ")
+print("Created by Gabriel H. @weekndr_sec")
 print("https://github.com/ndr-repo")
-print(' ')
+print(" ")
 print(" 'Knowledge is powerful, be careful how you use it!' ")
-print(' ')
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
-if len(sys.argv) < 2:
-        print('Please include the target username. ')
-        print(' ')
-        print('syntax: python irisLookup.py <username> ')
-        print(' ')
-        exit()
-targetUN = sys.argv[1]
-targetUser = str(targetUN)
-financeUrls = {
+print(" ")
+print("  ")
+urls = {
      "https://venmo.com/u/"+targetUser,
      "https://cash.app/$"+targetUser,
      "https://revolut.me/api/web-profile/"+targetUser,
-}
-socialUrls = {
+     "https://patreon.com/"+targetUser,
      "https://x.com/"+targetUser,
-     "https://"+targetUser+".tumblr.com",
      "https://youtube.com/@"+targetUser,
      "https://threads.net/@"+targetUser,
-     "https://rumble.com/c/"+targetUser,
-     "https://instagram.com/"+targetUser
-}
-datingUrls ={"https://tinder.com/@"+targetUser}
-devUrls ={
-     "https://pypi.org/user/"+targetUser,
-     "https://www.npmjs.com/~"+targetUser,
-     "https://github.com/"+targetUser,
-     "https://hub.docker.com/v2/users/"+targetUser,
-     "https://discuss.elastic.co/u/"+targetUser
-     }
-miscUrls ={
+     "https://instagram.com/"+targetUser,
      "https://hackerone.com/"+targetUser,
      "https://bugcrowd.com/"+targetUser,
-     "https://patreon.com/"+targetUser,
+     "https://github.com/"+targetUser,
+     "https://hub.docker.com/v2/users/"+targetUser,
+     "https://discuss.elastic.co/u/"+targetUser,
      "https://wordpress.org/support/users/"+targetUser,
+     "https://"+targetUser+".tumblr.com",
      "https://soundcloud.com/"+targetUser,
-
+     "https://rumble.com/c/"+targetUser,
+     "https://pypi.org/user/"+targetUser,
+     "https://www.npmjs.com/~"+targetUser
 }
+print(str("Target User: ") + str(targetUser))
 url_content = set()
-print(' ')
-print('Category: Finance ')
-for financeUrl in financeUrls:
-    response = requests.get(financeUrl, headers=headers)
-    reqUrl = response.request.url
-    if response.status_code == 200:
-        print(" - Profile found: ", reqUrl, end='\n')
-    if response.status_code == 403: 
-        print(" - Request denied by host: ", reqUrl, end='\n')
-print(' ')
-print('Category: Socials ')
-for socialUrl in socialUrls:
-    response = requests.get(socialUrl, headers=headers)
-    reqUrl = response.request.url
-    if response.status_code == 200:
-        print(" - Profile found: ", reqUrl, end='\n')
-    if response.status_code == 403: 
-        print(" - Request denied by host: ", reqUrl, end='\n')
-print(' ')
-print('Category: Online Dating ')
-for datingUrl in datingUrls:
-    response = requests.get(datingUrl, headers=headers)
-    reqUrl = response.request.url
-    if response.status_code == 200:
-        print(" - Profile found: ", reqUrl, end='\n')
-    if response.status_code == 403: 
-        print(" - Request denied by host: ", reqUrl, end='\n')
-print(' ')
-print('Category: Developer Sites ')
-for devUrl in devUrls:
-    response = requests.get(devUrl, headers=headers)
-    reqUrl = response.request.url
-    if response.status_code == 200:
-        print(" - Profile found: ", reqUrl, end='\n')
-    if response.status_code == 403: 
-        print(" - Request denied by host: ", reqUrl, end='\n')
-print(' ')
-print('Category: Misc ')
-for miscUrl in miscUrls:
-    response = requests.get(miscUrl, headers=headers)
-    reqUrl = response.request.url
-    if response.status_code == 200:
-        print(" - Profile found: ", reqUrl, end='\n')
-    if response.status_code == 403: 
-        print(" - Request denied by host: ", reqUrl, end='\n')
-print(" ")
+for url in urls:
+    try:
+        response = requests.get(url, headers=headers)
+        reqUrl = response.request.url
+        if response.status_code == 200:
+            logging.info(str("Profile found: ") + str(reqUrl))
+        if response.status_code == 403:
+            logging.error(str("Request denied by host: ") + str(reqUrl))
+        if response.status_code == 404:
+            logging.warning(str("No profile found: ") + str(reqUrl))
+    except:
+        requests.exceptions.SSLError
+        continue
 print("Scan complete.")
-print(" ")
-    # if response.status_code == 404:
-         # print("No profile found: ", reqUrl, end='\n')
